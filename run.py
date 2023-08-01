2008e6bcd2ec5befd9bb349a2c6a1507e285771c
Antonyeaster <>
2023-08-01 21:32:31 +0000

    Add function to create the deck

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 import time
 import sys
 import random
+from time import sleep
 
 print("""
 
@@ -15,11 +16,37 @@ $$    $$< $$ | $$$$$$  |/$$$$$$$/ $$ |_/$$/  __   $$ | $$$$$$  |/$$$$$$$/ $$ |_/
 $$$$$$$  |$$ | /    $$ |$$ |      $$   $$<  /  |  $$ | /    $$ |$$ |      $$   $$<  
 $$ |__$$ |$$ |/$$$$$$$ |$$ \_____ $$$$$$  \ $$ \__$$ |/$$$$$$$ |$$ \_____ $$$$$$  \ 
 $$    $$/ $$ |$$    $$ |$$       |$$ | $$  |$$    $$/ $$    $$ |$$       |$$ | $$  |
-$$$$$$$/  $$/  $$$$$$$/  $$$$$$$/ $$/   $$/  $$$$$$/   $$$$$$$/  $$$$$$$/ $$/   $$/ 
-
+$$$$$$$/  $$/  $$$$$$$/  $$$$$$$/ $$/   $$/  $$$$$$/   $$$$$$$/  $$$$$$$/ $$/   $$/
 """)
 
-print("Welcome to BlackJack")
+
+def type_text(text):
+    """
+    Creates typewriter effect for added expeirence
+    """
+    for i in text + "\n":
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.08)
+
+
+def create_deck():
+    """
+    Creates a deck of 52 cards to be picked at random
+    """
+    return [
+        2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K" "A",
+        2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K" "A",
+        2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K" "A",
+        2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K" "A"
+        ]  
+
+
+deck = create_deck()
+
+
+
+type_text("Welcome to BlackJack")
 
 username = input("Whats your name? ")
 print("Welcome", username, "lets have some fun")
